5abe4f1ddc8e65d0f410dd221f9763a1ffe2bd4a
Tomohiro Soga <tomohiro_soga@yahoo.co.jp>
2020-08-18 14:14:22 +0900

    fix listen host

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: deno run --allow-env --allow-net --allow-read index.ts --mode=api --port=${PORT}
+web: PORT=${PORT} deno run --allow-env --allow-net --allow-read index.ts --mode=api
--- http/index.ts
+++ http/index.ts
@@ -1,10 +1,8 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts';
-import { parse } from "https://deno.land/std/flags/mod.ts";
 import _404 from './handler/404.ts';
 import router from './router.ts';
 
-const args = parse(Deno.args);
-const port = args.port || Deno.env.get('PORT') || 8080;
+const port = Deno.env.get('PORT') || 8080;
 
 const app = new Application();
 
@@ -14,4 +12,4 @@ app.use(_404);
 
 console.log(`Listening on port:${port}`);
 
-await app.listen(`localhost:${port}`);
+await app.listen(`0.0.0.0:${port}`);
