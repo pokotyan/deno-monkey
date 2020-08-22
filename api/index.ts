b3c00c6d8ccec65d01f3e26c89088ab3d98e1e4a
Tomohiro Soga <tomohiro_soga@yahoo.co.jp>
2020-08-23 07:59:50 +0900

    setteing cors

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,13 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts';
-import _404 from './handler/404.ts';
-import router from './router.ts';
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import _404 from "./handler/404.ts";
+import router from "./router.ts";
 
-const port = Deno.env.get('PORT') || 8080;
+const port = Deno.env.get("PORT") || 8080;
 
 const app = new Application();
 
+app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(_404);
